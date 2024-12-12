b56aa1acc2a55dd2c763b3ef427cd9e87796d2d4
viona <vdevaqaulika@gmail.com>
2024-12-12 12:50:24 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -6,7 +6,25 @@ public class Nomor1 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
+        public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt(); 
 
+            for (int i = 0; i < n; i++) {
+                System.out.print("Masukkan nilai n: ");  
+                int N = input.nextInt();
+
+        if (n/3 == 0) {
+        System.out.println("Fizz");
+        }
+            else if (n/5 == 0 ) {
+               System.out.println("Buzz");
+                
+            }   else // angka kelipatan 3 && 5
+                System.out.println("FizzBuzz");
+        }
+    }
+}
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
