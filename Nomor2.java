282d5483cebf6a6882085d3b8ae460af157a6ffd
viona <vdevaqaulika@gmail.com>
2024-12-12 12:51:03 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -5,7 +5,41 @@ public class Nomor2 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
+public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        String transaksi = input.nextLine()
 
+       ArrayList<String> daftarBuku = new ArrayList<>();
+       Scanner scanner= new Scanner(System.in);
+       String Buku;
+
+       System.out.println("Masukkan pilihan buku [1/2/3/4]:");
+       String bukuString = input.nextLine();
+       input.nextLine();
+
+       String pilihan;
+
+        System.out.print("Input lagi [Y/N]? ");
+        pilihan = scanner.nextLine();
+
+        if (transaksi < 20000) {
+            System.out.println("tidak mendapat voucher");
+            int nilaiTransaksi = input.nextInt();  
+            
+            else if (transaksi >= 20000 && >=50000) 
+            System.out.println("voucher: 50000"); 
+            int nilaiTransaksi = input.nextInt();  
+
+            else {
+                System.out.println();
+
+            }
+}
+        
+
+     }
+}
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor2.class.getDeclaredMethods().length); 
